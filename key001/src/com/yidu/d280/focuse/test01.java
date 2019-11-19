9d1cc147ce8d6412f4884d4711da6ba1b0e50d43
dw2dengwei <dw2dengwei@163.com>
2019-11-19 14:58:48 +0800

    update key 002

--- key001/src/com/yidu/d280/focuse/test01.java
+++ key001/src/com/yidu/d280/focuse/test01.java
@@ -4,5 +4,8 @@ public class test01 {
 	public static void main(String[] age){
 		System.out.println("11111111");
 		System.out.println("22222222");
+		System.out.println("33333333");
+		System.out.println("44444444");
+		System.out.println("55555555");
 	}
 }
