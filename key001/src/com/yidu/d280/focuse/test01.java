8ed28d65aa16380ef8f9418a1c4a59b78d9e3e4c
dw2dengwei <dw2dengwei@163.com>
2019-11-19 14:49:46 +0800

    update 00

--- key001/src/com/yidu/d280/focuse/test01.java
+++ key001/src/com/yidu/d280/focuse/test01.java
@@ -1,7 +1,8 @@
 package com.yidu.d280.focuse;
 
 public class test01 {
-	public static void main(){
+	public static void main(String[] age){
 		System.out.println("11111111");
+		System.out.println("22222222");
 	}
 }
